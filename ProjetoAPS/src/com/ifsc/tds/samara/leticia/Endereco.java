2127c748fc2bbcca15706ec505a455a85c496387
LeticiaPassold <110414761+LeticiaPassold@users.noreply.github.com>
2022-12-13 15:15:14 -0300

    Implement class Endereco

--- ProjetoAPS/src/com/ifsc/tds/samara/leticia/Endereco.java
+++ ProjetoAPS/src/com/ifsc/tds/samara/leticia/Endereco.java
@@ -1,10 +1,49 @@
 package com.ifsc.tds.samara.leticia;
 
 public class Endereco {
+	private String rua;
+	private String cep;
+	private String cidade;
+	private String estado;
+	
+	public Endereco(){}
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public String toString() {
+		String string;
+    	string = "Rua = " + this.rua + "Cep = " + this.cep + "cidade = " + this.cidade + "Estado = " + this.estado;
+    	return string;
+	}
+	
+	public String getRua() {
+		return rua;
 	}
 
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
 }
