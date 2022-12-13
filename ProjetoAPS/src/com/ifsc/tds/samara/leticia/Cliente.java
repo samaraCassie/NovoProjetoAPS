1bd4f7e889a19e04217cf34ae9c3fd8eedaf9b56
LeticiaPassold <110414761+LeticiaPassold@users.noreply.github.com>
2022-12-13 15:12:55 -0300

    Implement class Cliente

--- ProjetoAPS/src/com/ifsc/tds/samara/leticia/Cliente.java
+++ ProjetoAPS/src/com/ifsc/tds/samara/leticia/Cliente.java
@@ -1,10 +1,36 @@
 package com.ifsc.tds.samara.leticia;
-//dddddd
+
 public class Cliente {
+	private int clienteId;
+    private String nome;
+    private Endereco endereco;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public Cliente() {}
 
-	}
+    public String toString() {
+     String string;
+     string = "ID = " + this.clienteId + "Nome = " + this.nome + "Endereco = " + this.endereco;
+    	
+        return string;
+    }
+
+    public int getClienteId() {
+        return clienteId;
+    }
+    public void setClienteId(int clienteId) {
+        this.clienteId = clienteId;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 
 }
