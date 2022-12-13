1e0cf9ebd23a263723729516eca7e2dd6e058ef1
LeticiaPassold <110414761+LeticiaPassold@users.noreply.github.com>
2022-12-13 16:45:39 -0300

    Implemet class PessoaFisica

--- ProjetoAPS/src/com/ifsc/tds/samara/leticia/PessoaFisica.java
+++ ProjetoAPS/src/com/ifsc/tds/samara/leticia/PessoaFisica.java
@@ -1,5 +1,24 @@
 package com.ifsc.tds.samara.leticia;
 
-public class PessoaFisica {
+public class PessoaFisica extends Cliente{
+	private String cpf;
+	
+	public PessoaFisica() {}
+	
+	public String toString() {
+    	String string;
+    	string = "CPF = " + this.cpf;
+    	return string;
+    }
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
 
 }
