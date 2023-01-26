9ce29ca2ace713cb05a3dbe39eb2b9d3ffe93c5f
samaraCassie <samaracassie@outlook.com>
2023-01-25 23:56:19 -0300

    Create Lista.java

---
+++ ProjetoAPS/src/com/ifsc/tds/samara/leticia/Lista.java
@@ -0,0 +1,10 @@
+package com.ifsc.tds.samara.leticia;
+
+public class Lista extends Cliente{
+
+    public Lista(int id, String n, Endereco e) {
+        super(id, n, e);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
